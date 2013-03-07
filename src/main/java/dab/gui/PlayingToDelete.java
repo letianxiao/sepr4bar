5bbc41e11ad17305a14d09af46e74b042e5318b0
Aiste <aiste.kiskyte@gmail.com>
2013-03-07 21:11:06 +0000

    Just testing
    new file:   src/main/java/dab/gui/PlayingToDelete.java

---
+++ src/main/java/dab/gui/PlayingToDelete.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dab.gui;
+
+/**
+ *
+ * @author Aiste
+ */
+public class PlayingToDelete {
+    String abc;
+}
